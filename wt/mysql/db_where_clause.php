5e5ea9417a8cb68fc27a0f18779ed468dd943584
Prof. Tushar Gohil <111980549+tgohil-logilite@users.noreply.github.com>
2023-10-09 09:34:39 +0530

    Update db_where_clause.php

--- wt/mysql/db_where_clause.php
+++ wt/mysql/db_where_clause.php
@@ -1,17 +1,11 @@
 <?php
-$con=mysqli_connect("localhost","root","","test1");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$connect = mysqli_connect("localhost","root","","people");
+$sql = "select * from contact where pid = 1";
+    $result = $connect->query($sql);
 
-$query = "SELECT * FROM Persons WHERE FirstName='Peter'";
-$result = mysqli_query($con, $query);
-
-while($row = mysqli_fetch_array($result))
-  {
-  echo $row['FirstName'] . " " . $row['LastName'];
-  echo "<br>";
-  }
+    while($row = mysqli_fetch_array($result))
+    {
+	echo($row["pid"]."-".$row["pname"]."-".$row["pemail"]);
+    echo("<br>");
+    }
 ?>

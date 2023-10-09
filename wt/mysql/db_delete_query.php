c738b30b19944caf69c4e027ff1a367f3821a649
Prof. Tushar Gohil <111980549+tgohil-logilite@users.noreply.github.com>
2023-10-09 09:37:09 +0530

    Update db_delete_query.php

--- wt/mysql/db_delete_query.php
+++ wt/mysql/db_delete_query.php
@@ -1,13 +1,28 @@
 <?php
-$con=mysqli_connect("localhost","root","","test1");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$connect = mysqli_connect("localhost","root","","people");
 
-$query = "DELETE FROM Persons WHERE LastName='Griffin'";
-mysqli_query($con, $query);
+// To delete data from a database table
+$query = 'delete from contact where pid=1';
+$connect->query($query);
+echo("Record Deleted with pid = 1 from contact table !!!!");
 
-mysqli_close($con);
+$sql = "select * from contact";
+    $result = $connect->query($sql);
+
+    while($row = mysqli_fetch_array($result))
+    {
+	echo($row["pid"]."-".$row["pname"]."-".$row["pemail"]);
+    echo("<br>");
+    }
+
+// To delete a database
+$query = 'drop database people'; 
+ 	
+// To delete a column from a table
+$query = 'alter table contact drop pemail';
+
+// To destroy a whole table
+$query = 'drop table contact';
+
+
 ?>
